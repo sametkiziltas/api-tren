1a3b4efc45276877558bf162e7cb0e0ab12bf390
sametkiziltas <samet-kiziltas@outlook.com>
2023-09-01 22:09:05 +0300

    migrations added

---
+++ src/tren.api/Migrations/20230901190621_first.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace tren.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "words",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    turkish = table.Column<string>(type: "text", nullable: false),
+                    english = table.Column<string>(type: "text", nullable: false),
+                    category = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_words", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "words");
+        }
+    }
+}
