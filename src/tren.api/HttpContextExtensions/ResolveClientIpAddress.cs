333b1941862b1953c161627c8062d2cd05eb734c
sametkiziltas <samet-kiziltas@outlook.com>
2023-09-11 19:39:22 +0300

    global rate limiting

---
+++ src/tren.api/HttpContextExtensions/ResolveClientIpAddress.cs
@@ -0,0 +1,17 @@
+namespace tren.api.HttpContextExtensions;
+
+internal static class HttpContextExtensions
+{
+    public static string ResolveClientIpAddress(this HttpContext httpContext)
+    {
+        var cfViewerAddress = httpContext.Request.Headers["CloudFront-Viewer-Address"];
+        if (cfViewerAddress.Count > 0)
+        {
+            var parts = (cfViewerAddress[0] ?? string.Empty).Split(":");
+            if (parts.Length >= 1)
+                return string.Join(",", parts[0..^1]);
+        }
+
+        return httpContext.Connection.RemoteIpAddress?.ToString() ?? "";
+    }
+}
--- src/tren.api/Program.cs
+++ src/tren.api/Program.cs
@@ -1,13 +1,37 @@
+using System.Threading.RateLimiting;
 using Microsoft.EntityFrameworkCore;
 using tren.api.Api.Data;
 using tren.api.Api.Data.Repositories;
 using tren.api.Api.Services;
+using tren.api.HttpContextExtensions;
 using tren.api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = 429;
+    options.GlobalLimiter = PartitionedRateLimiter.CreateChained(
+        PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+            RateLimitPartition.GetFixedWindowLimiter(httpContext.ResolveClientIpAddress(), partition =>
+                new FixedWindowRateLimiterOptions
+                {
+                    AutoReplenishment = true,
+                    PermitLimit = 10,
+                    Window = TimeSpan.FromMinutes(1)//10 request per minute
+                })),
+        PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+            RateLimitPartition.GetFixedWindowLimiter(httpContext.ResolveClientIpAddress(), partition =>
+                new FixedWindowRateLimiterOptions
+                {
+                    AutoReplenishment = true,
+                    PermitLimit = 6000,
+                    Window = TimeSpan.FromHours(1)//6000 request per hour
+                })));
+});
 
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -29,6 +53,8 @@ builder.Services.AddLogging();
 builder.Services.AddControllers();
 
 var app = builder.Build();
+app.UseRateLimiter();
+
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
